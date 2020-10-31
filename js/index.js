66257854e13160d17caca402edefccb6abe68e4f
vincent0628 <vincent0910628007@gmail.com>
2020-10-31 13:13:21 +0800

    add bg

--- js/index.js
+++ js/index.js
@@ -5,13 +5,17 @@
   var card = $('card'),
       openB = $('open'),
       closeB = $('close'),
-      myAudio = $('myAudio'),
       timer = null;
-  
+
+  const myAudio = [$('myAudio1'),$('myAudio2')]
+  var randomSelection = Math.floor(Math.random() * (myAudio.length));                
+
   openB.addEventListener('click', function () {
     var password = prompt("Please enter password", "Anniversary, 4 digits");
     if (password == '0722') {
-      myAudio.play();
+      myAudio[randomSelection].play();
+      if (randomSelection ==0){myAudio[randomSelection+1].style.display="none";}
+      else{myAudio[randomSelection-1].style.display="none";}
       card.setAttribute('class', 'open-half');
       if (timer) clearTimeout(timer);
       timer = setTimeout(function () {
